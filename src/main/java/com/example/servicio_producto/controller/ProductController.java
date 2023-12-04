a2cdb6e89233953923c0575c983a8de5175441e2
Lizbeth Aquisse Castillo <74085459+LiziAC05@users.noreply.github.com>
2023-12-04 18:49:53 -0500

    .

---
+++ src/main/java/com/example/servicio_producto/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.example.servicio_producto.controller;
+
+import com.example.servicio_producto.entity.Product;
+import com.example.servicio_producto.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ProductController {
+    @Autowired
+    ProductRepository productRepository;
+    @GetMapping("/productos")
+    public List<Product> listar(){
+        return productRepository.findAll();
+    }
+    @GetMapping("/productos/{id}")
+    public Product buscarPorId(@PathVariable("id") int id){
+        Optional<Product> optional = productRepository.findById(id);
+        return optional.orElse(null);
+    }
+}
---
+++ src/main/java/com/example/servicio_producto/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.servicio_producto.repository;
+
+import com.example.servicio_producto.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RepositoryRestResource(path = "product")
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
